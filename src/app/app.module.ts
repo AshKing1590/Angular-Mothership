6e638b9674d85a278fdcb6dc4158843f44e6a8de
AshKing1590 <ashish.kateryar@gmail.com>
2018-08-12 21:32:34 +0530

    first check in

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
+import {AgGridModule} from 'ag-grid-angular';
+import {SidebarModule} from 'primeng/sidebar';
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, 
+    AgGridModule.withComponents([]),
+    BrowserAnimationsModule,
+    SidebarModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
